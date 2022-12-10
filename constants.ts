3fdb2f73155201bba0d00320496520baaadcb2dc
shubh patni <shubhpatni2002@gmail.com>
2022-12-10 15:40:00 -0500

    first commit

---
+++ constants.ts
@@ -0,0 +1,257 @@
+import { Location } from "@prisma/client";
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthsStr = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+// A function to run at Specified Time(5:00AM EST) everyday
+// get the today's date, day, month, year
+
+export enum Day {
+  Sun = "Sun",
+  Mon = "Mon",
+  Tue = "Tue",
+  Wed = "Wed",
+  Thu = "Thu",
+  Fri = "Fri",
+  Sat = "Sat",
+}
+
+const gymToFile = {
+  Marino2Floor: [
+    "./data/MarinoCenter2Floor.json",
+    "./data/moreData/MarinoCenter2Floor.csv",
+  ],
+  Marino3Floor: [
+    "./data/MarinoCenter3FloorWeight.json",
+    "./data/moreData/MarinoCenter3FloorWeight.csv",
+  ],
+  MarinoGymnasium: [
+    "./data/MarinoCenterGym.json",
+    "./data/moreData/MarinoCenterGym.csv",
+  ],
+  MarinoTrack: ["./data/MarinoTrack.json", "./data/moreData/MarinoTrack.csv"],
+  MarinoCardio: [
+    "./data/MarinoCenter3FloorCardio.json",
+    "./data/moreData/MarinoCenter3FloorCardio.csv",
+  ],
+  SquashBusters: [
+    "./data/MarinoSquashBusters.json",
+    "./data/moreData/MarinoSquashBusters.csv",
+  ],
+};
+
+const allGyms = [
+  "Marino2Floor",
+  "MarinoGymnasium",
+  "Marino3Floor",
+  "MarinoCardio",
+  "MarinoTrack",
+  "SquashBusters",
+];
+
+export const Timings = {
+  MarinoEndTime: "23:49",
+  MarinoWeekdayStartTime: "5:30",
+  MarinoWeekendStartTime: "8:00",
+  SquashWeekdayStartTime: "6:00",
+  SquashWeekdayEndTime: "23:49",
+  SquashWeekendEndTime: "21:00",
+  SquashSatStartTime: "08:00",
+  SquashSunStartTime: "10:00",
+};
+
+const csvFields = [
+  "Date",
+  "Day",
+  "Min",
+  "Max",
+  "0:00",
+  "0:30",
+  "1:00",
+  "1:30",
+  "2:00",
+  "2:30",
+  "3:00",
+  "3:30",
+  "4:00",
+  "4:30",
+  "5:00",
+  "5:30",
+  "6:00",
+  "6:30",
+  "7:00",
+  "7:30",
+  "8:00",
+  "8:30",
+  "9:00",
+  "9:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+  "20:30",
+  "21:00",
+  "21:30",
+  "22:00",
+  "22:30",
+  "23:00",
+  "23:30",
+];
+
+export function dayToString(day: Day) {
+  if (day == Day.Sun) {
+    return "Sun";
+  } else if (day == Day.Mon) {
+    return "Mon";
+  } else if (day == Day.Tue) {
+    return "Tue";
+  } else if (day == Day.Wed) {
+    return "Wed";
+  } else if (day == Day.Thu) {
+    return "Thu";
+  } else if (day == Day.Fri) {
+    return "Fri";
+  } else if (day == Day.Sat) {
+    return "Sat";
+  }
+}
+
+export function stringToDay(str: string): Day {
+  if (str == "Sun") {
+    return Day.Sun;
+  } else if (str == "Mon") {
+    return Day.Mon;
+  } else if (str == "Tue") {
+    return Day.Tue;
+  } else if (str == "Wed") {
+    return Day.Wed;
+  } else if (str == "Thu") {
+    return Day.Thu;
+  } else if (str == "Fri") {
+    return Day.Fri;
+  } else if (str == "Sat") {
+    return Day.Sat;
+  } else {
+    return Day.Sun;
+  }
+}
+
+export const timeStringToMinutes = (timeStr: string, separator: string) =>
+  timeStr
+    .split(separator)
+    .reduce((h: string, m: string) => parseInt(h) * 60 + m);
+
+export const minutesToTimeString = (minutes: number, separator: string) => {
+  const minutesPart = (minutes % 60).toString().padStart(2, "0");
+  const hoursPart = Math.floor(minutes / 60)
+    .toString()
+    .padStart(2, "0");
+  return hoursPart + separator + minutesPart;
+};
+
+export const url =
+  "https://connect2concepts.com/connect2/?type=circle&key=2A2BE0D8-DF10-4A48-BEDD-B3BC0CD628E7";
+
+export function roundToNearest30(time: string) {
+  const t = time.split(":");
+  if (parseInt(t[1]) < 30) {
+    return `${t[0]}:00`;
+  } else {
+    return `${t[0]}:30`;
+  }
+}
+
+export const timeOptions: Intl.DateTimeFormatOptions = {
+  timeZone: "America/New_York",
+  hour12: false,
+  timeStyle: "short",
+};
+
+export const dayOptions: Intl.DateTimeFormatOptions = {
+  timeZone: "America/New_York",
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+
+  weekday: "short",
+};
+
+//Don't really need files anymore Shubh, what do we do here instead now?
+// Idk how to write a for loop in TS lol plz look it up
+// async function createFields(file) {
+//   const savedDataFile = await fs.readFileSync(file);
+//   let gymData = JSON.parse(savedDataFile);
+
+//   for (var i = 4; i < csvFields.length; i++) {
+//     for (each of daysOfWeek) {
+//       if (!gymData[each][csvFields[i]]) {
+//         gymData[each][csvFields[i]] = { Average: 0, Count: 0, CurrentCount: 0 };
+//       }
+//     }
+//   }
+//   await fs.writeFileSync(file, JSON.stringify(gymData));
+// }
+
+//FROM STACKOVERFLOW
+
+export function generateTimeSlots(
+  startStr: string,
+  endStr: string,
+  periodInMinutes: number,
+  separator = ":"
+) {
+  let startMinutes = timeStringToMinutes(startStr, separator);
+  let endMinutes = timeStringToMinutes(endStr, separator);
+  const oneDayInMinutes = 1440;
+  const endMinutesUpdated: number =
+    parseInt(endMinutes) >= oneDayInMinutes
+      ? oneDayInMinutes - 1
+      : parseInt(endMinutes);
+  // if (parseInt(endMinutes) >= oneDayInMinutes) {
+  //   const endMinutes = oneDayInMinutes - 1;
+  //if (startMinutes <= 0) startMinutes = 0;
+
+  const startMinutesUpdated: number =
+    parseInt(startMinutes) <= 0 ? 0 : parseInt(startMinutes);
+
+  return Array.from(
+    {
+      length:
+        Math.floor(
+          (endMinutesUpdated - startMinutesUpdated) / periodInMinutes
+        ) + 1,
+    },
+    (_, i) =>
+      minutesToTimeString(startMinutesUpdated + i * periodInMinutes, separator)
+  );
+}
+
+//new Date().toLocaleString("en-US",
---
+++ pages/api/scraper.ts
@@ -0,0 +1,108 @@
+// pages/api/cron.ts
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { verifySignature } from "@upstash/qstash/nextjs";
+import { scraper } from "../../scraper";
+import { save } from "../../storeData";
+import { Location } from "@prisma/client";
+import { Day } from "../../constants";
+import {
+  dayOptions,
+  timeOptions,
+  roundToNearest30,
+  Timings,
+  dayToString,
+  stringToDay,
+} from "../../constants";
+//random comment
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      //Day
+      const wholeDate = new Date().toLocaleString("en-US", dayOptions); //Whole day looks like 'Wed, 9/21/2022'
+      const day = wholeDate.split(",")[0];
+      const dayUpdated: Day = day ? stringToDay(day) : Day.Sun;
+
+      //Count
+      let personCount: Array<number> = (await scraper()) as Array<number>;
+      console.log(personCount);
+
+      //Time
+      const CT = new Date().toLocaleString("en-US", timeOptions); // current Time
+      const roundedTime = roundToNearest30(CT);
+
+      console.log("running Saves");
+      await save(
+        Location.Marino2Floor,
+        personCount[0],
+        roundedTime,
+        dayUpdated
+      );
+      await save(
+        Location.MarinoGymnasium,
+        personCount[1],
+        roundedTime,
+        dayUpdated
+      );
+      await save(
+        Location.Marino3Floor,
+        personCount[2],
+        roundedTime,
+        dayUpdated
+      );
+      await save(
+        Location.MarinoCardio,
+        personCount[3],
+        roundedTime,
+        dayUpdated
+      );
+      await save(Location.MarinoTrack, personCount[4], roundedTime, dayUpdated);
+
+      if (["Mon", "Tue", "Wed", "Thu", "Fri"].includes(day)) {
+        if (roundedTime >= Timings.SquashWeekdayStartTime) {
+          await save(
+            Location.SquashBusters,
+            personCount[5],
+            roundedTime,
+            dayUpdated
+          );
+        }
+      } else {
+        if (day == "Sat") {
+          if (roundedTime >= Timings.SquashSatStartTime) {
+            await save(
+              Location.SquashBusters,
+              personCount[5],
+              roundedTime,
+              dayUpdated
+            );
+          }
+        } else {
+          if (roundedTime >= Timings.SquashSunStartTime) {
+            await save(
+              Location.SquashBusters,
+              personCount[5],
+              roundedTime,
+              dayUpdated
+            );
+          }
+        }
+      }
+
+      res.status(200).json({ success: true });
+    } catch ({ message }) {
+      res.status(500).json({ statusCode: 500, message: message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
+
+export default verifySignature(handler);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
